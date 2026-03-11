3adce512330674d9bc38360bb2155302fc5c4acb
ckranig <ckranig@wisc.edu>
2026-03-11 11:38:20 -0500

    added intall script for powerdns resolver

---
+++ powerdns/resolver/install.sh
@@ -0,0 +1,4 @@
+echo "deb [signed-by=/etc/apt/keyrings/rec-54-pub.asc] http://repo.powerdns.com/ubuntu jammy-rec-54 main" | sudo tee /etc/apt/sources.list.d/pdns.list
+printf "Package: pdns-*\nPin: origin repo.powerdns.com\nPin-Priority: 600\n" | sudo tee /etc/apt/preferences.d/rec-54 > /dev/null
+sudo install -d /etc/apt/keyrings; curl https://repo.powerdns.com/FD380FBB-pub.asc | sudo tee /etc/apt/keyrings/rec-54-pub.asc
+sudo apt-get update && sudo apt-get install pdns-recursor
