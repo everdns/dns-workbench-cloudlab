39e6f94de2e27c5f31b058902794ebd68c7f3136
ckranig <ckranig@wisc.edu>
2026-03-16 10:54:09 -0500

    Added start script for dns software

---
+++ start_dns.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+    echo "Usage: $0 <software>"
+    echo "Options: bind-resolver, bind-ns, powerdns-resolver, powerdns-ns, knot-resolver, knot-ns, nsd-ns, unbound-resolver"
+    exit 1
+fi
+
+case "$1" in
+    bind-resolver)
+        /local/repository/bind/resolver/start.sh
+        ;;
+    bind-ns)
+        /local/repository/bind/ns/start.sh
+        ;;
+    powerdns-resolver)
+        /local/repository/powerdns/resolver/start.sh
+        ;;
+    powerdns-ns)
+        /local/repository/powerdns/ns/start.sh
+        ;;
+    knot-resolver)
+        /local/repository/knot/resolver/start.sh
+        ;;
+    knot-ns)
+        /local/repository/knot/ns/start.sh
+        ;;
+    nsd-ns)
+        /local/repository/nsd/ns/start.sh
+        ;;
+    unbound-resolver)
+        /local/repository/unbound/resolver/start.sh
+        ;;
+    *)
+        echo "Unknown software: $1"
+        echo "Options: bind-resolver, bind-ns, powerdns-resolver, powerdns-ns, knot-resolver, knot-ns, nsd-ns, unbound-resolver"
+        exit 1
+        ;;
+esac
