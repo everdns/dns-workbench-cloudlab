72b18156efd060172843a83c358d5b27fdf50351
ckranig <ckranig@wisc.edu>
2026-03-16 12:58:22 -0500

    fixed issue with copying files for unbound ns

--- unbound/ns/install.sh
+++ unbound/ns/install.sh
@@ -5,9 +5,10 @@ sudo apt install -y build-essential libssl-dev libexpat1-dev bison flex
 sudo wget https://nlnetlabs.nl/downloads/unbound/unbound-1.24.2.tar.gz -O /opt/unbound-1.24.2.tar.gz
 sudo tar -xzf /opt/unbound-1.24.2.tar.gz
 cd /opt/unbound-1.24.2 && sudo ./configure && sudo make && sudo make install
-sudo cp /local/repository/unbound/ns/unbound.conf /usr/local/etc/unbound/unbound.conf
 if [ "$MULTIPLE_IFACE" = true ]; then
     sudo cp /local/repository/unbound/ns/unbound2.conf /usr/local/etc/unbound/unbound.conf
+else
+    sudo cp /local/repository/unbound/ns/unbound.conf /usr/local/etc/unbound/unbound.conf
 fi
 sudo cp /local/repository/zone_file_defaults/db.workbench.lan /usr/local/etc/unbound/db.workbench.lan
 sudo cp /local/repository/zone_file_defaults/db.dns64perf.test /usr/local/etc/unbound/db.dns64perf.test
