84882846e94de03097ccb80f4561ea922187d76b
ckranig <ckranig@wisc.edu>
2026-04-27 10:11:39 -0500

    updated unbound install

--- unbound/ns/install.sh
+++ unbound/ns/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MULTIPLE_IFACE="${1:-false}"
-sudo apt install -y build-essential libssl-dev libexpat1-dev bison flex
+sudo apt install -y build-essential libssl-dev libexpat1-dev bison flex libevent-dev
 sudo wget https://nlnetlabs.nl/downloads/unbound/unbound-1.24.2.tar.gz -O /opt/unbound-1.24.2.tar.gz
 sudo tar -xzf /opt/unbound-1.24.2.tar.gz -C /opt
 cd /opt/unbound-1.24.2 && sudo ./configure --with-libevent && sudo make && sudo make install
