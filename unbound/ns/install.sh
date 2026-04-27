2a75a4ae93fae2a170a05e3119f93f0277908fbe
ckranig <ckranig@wisc.edu>
2026-04-27 10:10:24 -0500

    updated unbound config

--- unbound/ns/install.sh
+++ unbound/ns/install.sh
@@ -3,7 +3,7 @@ MULTIPLE_IFACE="${1:-false}"
 sudo apt install -y build-essential libssl-dev libexpat1-dev bison flex
 sudo wget https://nlnetlabs.nl/downloads/unbound/unbound-1.24.2.tar.gz -O /opt/unbound-1.24.2.tar.gz
 sudo tar -xzf /opt/unbound-1.24.2.tar.gz -C /opt
-cd /opt/unbound-1.24.2 && sudo ./configure && sudo make && sudo make install
+cd /opt/unbound-1.24.2 && sudo ./configure --with-libevent && sudo make && sudo make install
 if [ "$MULTIPLE_IFACE" = true ]; then
     sudo cp /local/repository/unbound/ns/unbound2.conf /usr/local/etc/unbound/unbound.conf
 else
